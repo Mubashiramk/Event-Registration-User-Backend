8c7ad47bf7e23ab9af8293edf593d3e778fc3138
Mubashira M K <mubashiramk07@gmail.com>
2022-05-26 00:32:50 +0530

    RealTime with Socket.io

--- index.js
+++ index.js
@@ -7,10 +7,33 @@ const routesUrls = require("./routes/routes");
 const dataRouter = require("./routes/adminRoutes");
 const generateUploadURL = require("./s3.js");
 
+//add socketio
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    methods: ["GET", "POST"],
+  },
+});
+//end socketio
+
 dotenv.config();
 mongoose.connect(process.env.DATABASE_ACCESS, () =>
   console.log("Database Connected")
 );
+
+//add socketio
+io.on("connection", (socket) => {
+  socket.emit("hello", "world");
+  socket.on("on", (arg) => {
+    socket.emit("hello", "world");
+    console.log(arg);
+  });
+  console.log("a user connected");
+});
+
+//end socketio
 
 app.use(express.json());
 app.use(cors());
@@ -20,7 +43,7 @@ app.get("/s3url", async (req, res) => {
   const url = await generateUploadURL();
   res.send({ url });
 });
-app.listen(4000, () => console.log("server is running"));
+server.listen(4000, () => console.log("server is running")); //changed app to server
 app.get("/", (req, res) => {
   res.send("<h1>user</h1>");
 });
